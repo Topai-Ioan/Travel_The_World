e5b4681eabae80ea9fe208273d05982d41c6ca48
Alexandru Topai <alexandru.topai@piamedia.com>
2023-08-04 13:04:04 +0300

    login and sign up pages

--- lib/consts.dart
+++ lib/consts.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 
-const backGroundColor = Color.fromRGBO(0, 0, 0, 1.0);
+const backgroundColor = Color.fromRGBO(0, 0, 0, 1.0);
 const blueColor = Color.fromRGBO(0, 149, 246, 1);
 const primaryColor = Colors.white;
 const secondaryColor = Colors.grey;
 const darkGreyColor = Color.fromRGBO(97, 97, 97, 1);
+const greenColor = Colors.green;
 
 Widget sizeVertical(double height) {
   return SizedBox(
---
+++ lib/features/presentation/pages/credential/sign_in_page.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:travel_the_world/consts.dart';
+import 'package:travel_the_world/features/presentation/pages/credential/sign_up_page.dart';
+import 'package:travel_the_world/features/presentation/widgets/form_container_widget.dart';
+import 'package:travel_the_world/features/presentation/widgets/button_container_widget.dart';
+
+class SignInPage extends StatelessWidget {
+  const SignInPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: backgroundColor,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Flexible(flex: 2, child: Container()),
+            Center(
+              child: SvgPicture.asset(
+                "assets/images/logo.svg",
+                colorFilter:
+                    const ColorFilter.mode(Colors.green, BlendMode.srcIn),
+              ),
+            ),
+            sizeVertical(30),
+            const FormContainerWidget(
+              hintText: "Email",
+            ),
+            sizeVertical(15),
+            const FormContainerWidget(
+              hintText: "Password",
+              isPasswordField: true,
+            ),
+            sizeVertical(15),
+            ButtonContainerWidget(
+              color: blueColor,
+              text: "Sign In",
+              onTapListener: () {},
+            ),
+            Flexible(flex: 2, child: Container()),
+            const Divider(
+              color: greenColor,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  "Don't have and account? ",
+                  style: TextStyle(color: primaryColor),
+                ),
+                InkWell(
+                  onTap: () {
+                    Navigator.pushAndRemoveUntil(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const SignUpPage()),
+                        (route) => false);
+                  },
+                  child: const Text(
+                    "Sign Up.",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold, color: primaryColor),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
