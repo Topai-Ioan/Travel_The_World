c966c5b90f93bd6314581bcbaf6428e1c226a0f0
Alexandru Topai <alexandru.topai@qverse.app>
2023-09-18 00:34:03 +0300

    bug fixed when deleting a photo

--- lib/features/presentation/pages/profile/profile_page.dart
+++ lib/features/presentation/pages/profile/profile_page.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:travel_the_world/features/domain/entites/user/user_entity.dart';
-import 'package:travel_the_world/features/presentation/cubit/post/post_cubit.dart';
 import 'package:travel_the_world/features/presentation/pages/profile/widgets/profile_main_widget.dart';
-import 'package:travel_the_world/injection_container.dart' as di;
 
 class ProfilePage extends StatelessWidget {
   final UserEntity currentUser;
@@ -11,9 +8,6 @@ class ProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider<PostCubit>.value(
-      value: di.sl<PostCubit>(),
-      child: ProfileMainWidget(currentUser: currentUser),
-    );
+    return ProfileMainWidget(currentUser: currentUser);
   }
 }
